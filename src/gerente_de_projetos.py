e14927404b556759e831baf75158a11a54fd2ea1
Rafael Feitosa <rafaelfeit@hotmail.com>
2025-06-10 18:52:03 -0300

    atualização do arquivo 'gerente_de_projetos'

--- src/gerente_de_projetos.py
+++ src/gerente_de_projetos.py
@@ -1,35 +1,47 @@
+class Projeto():
+    def __init__(self, nome, descricao, gerente=None):
+        self.nome = nome
+        self.descricao = descricao
+        self.gerente = gerente
+
+    def __str__(self):
+        if self.gerente:
+            return f"Projeto: {self.nome} - {self.descricao} foi adicionado ao(á) gerente: {self.gerente}"
+        return f"Projeto: {self.nome} - {self.descricao}"
+
+
 class GerenteProjetos:
     def __init__(self, nome, projetos=None):
         if projetos is None:
             projetos = []
-            self.nome = nome
-            self.projetos = projetos
+        self.nome = nome
+        self.projetos = projetos
 
     def adicionar_projeto(self, projeto):
+        projeto = Projeto(projeto.nome, projeto.descricao, self.nome)
         self.projetos.append(projeto)
-        print(f"O projeto '{projeto}' foi adicionado ao(á) gerente: {self.nome}")
 
     def listar_projetos(self):
         if self.projetos:
             print(f"\nProjeto do gerente: {self.nome} - Total de projetos: {len(self.projetos)}")
             for projeto in self.projetos:
-                print(f"- {projeto}")
+                print(projeto)
 
+
+projeto1 = Projeto("Sistema de vendas", "Desenvolvimento de um sistema para vendas online.")
+projeto2 = Projeto("App de Finanças", "Aplicativo para controle financeiro pessoal.")
 
 gerente = GerenteProjetos("Carlos")
-gerente.adicionar_projeto("Projeto A")
-gerente.adicionar_projeto("Projeto B")
-gerente.adicionar_projeto("Projeto C")
-gerente.adicionar_projeto("Projeto D")
+gerente.adicionar_projeto(projeto1)
+gerente.adicionar_projeto(projeto2)
 gerente.listar_projetos()
 
 print("\n===========================================================")
 
+projeto3 = Projeto("App Fintech", "Aplicativo de banco digital.")
+
 gerente2 = GerenteProjetos("Rafael")
-gerente2.adicionar_projeto("Projeto Java")
-gerente2.adicionar_projeto("Projeto Python")
-gerente2.adicionar_projeto("Projeto de API")
-gerente2.adicionar_projeto("Projeto Web")
+gerente2.adicionar_projeto(projeto3)
 gerente2.listar_projetos()
 
 print("===========================================================")
