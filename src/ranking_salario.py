cb9e68c7549264aeb33d2d49a2d1b5ad2cbab324
Rafael Feitosa <rafaelfeit@hotmail.com>
2025-06-10 19:54:13 -0300

    adicionando arquivo 'ranking salario'

---
+++ src/ranking_salario.py
@@ -0,0 +1,22 @@
+import random
+
+qtd = 10
+
+salario = [random.uniform(1518, 2500) for i in range(qtd)]
+
+salario_ordem = sorted(salario)
+salario_formato = [round(i, 2) for i in salario_ordem]
+
+print(f"Todos os salários - Total: {len(salario_formato)}")
+for i in salario_formato:
+    print(f"- R${i}")
+
+print("=" * 45)
+
+salarios_maiores_2000 = [s for s in salario if s > 2000]
+salarios_maiores_2000 = sorted(salarios_maiores_2000)
+salario_formato = [round(i, 2) for i in salarios_maiores_2000]
+
+print(f"Todos salários acima de R$ 2.000,00 - Total: {len(salarios_maiores_2000)}")
+for i in salario_formato:
+    print(f"- R${i}")
