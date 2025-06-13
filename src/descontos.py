c3a270c5ca38c81c8b2745bf471a9e9889d95875
Rafael Feitosa <rafaelfeit@hotmail.com>
2025-06-13 20:43:24 -0300

    atualização do arquivo 'desconto'

--- src/descontos.py
+++ src/descontos.py
@@ -1,15 +1,26 @@
+def validar_entrada(valor):
+    return float(valor.replace(",", "."))
+
+
 descontos = {
     "DESCONTO10": 0.10,
     "DESCONTO20": 0.20,
-    "SEM_DESCONTO": 0.00
+    "DESCONTO30": 0.30
 }
 
-preco = float(input("Digite o preço do produto: ").strip())
-cupom = input("Digite o cupom de desconto: ").strip().upper().replace(" ", "")
+try:
+    preco = validar_entrada(input("Digite o preço do produto: ").strip())
+    cupom = input("Digite o cupom de desconto: ").strip().upper().replace(" ", "")
 
-if cupom in descontos:
-    desconto = descontos[cupom]
-    preco_final = preco * (1 - desconto)
-    print(f"{preco_final:.2f}")
-else:
-    print("Cupom inválido")
+    if cupom in descontos:
+        desconto = descontos[cupom]
+        preco_final = preco * (1 - desconto)
+        print(f"====== Total: R${preco_final:.2f} ======")
+    elif cupom == "":
+        preco_final = preco
+        print(f"====== Total: R${preco_final:.2f} ======")
+    else:
+        print("Cupom inválido")
+
+except ValueError:
+    print("Entrada inválida!")
