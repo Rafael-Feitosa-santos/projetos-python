51f2b5c90200d0bcb199749904308852135eef94
Rafael Feitosa <rafaelfeit@hotmail.com>
2025-11-17 22:57:49 -0300

    Atualização do arquivo Atualizar planilha automaticamente com python

--- src/Atualizar planilha automaticamente com Python/main.py
+++ src/Atualizar planilha automaticamente com Python/main.py
@@ -1,7 +1,7 @@
 import openpyxl
 import os
 
-dados_xlsx = r"C:\Users\rafae\OneDrive\Área de Trabalho\teste-python1\dados.xlsx"
+dados_xlsx = r"C:/Users/rafae/OneDrive/Área de Trabalho/Atualizar planilha automaticamente com Python/dados.xlsx"
 
 def atualizar_planilha(nome, idade, cargo):
     # Verifica se o arquivo já existe
@@ -17,6 +17,13 @@ def atualizar_planilha(nome, idade, cargo):
         workbook = openpyxl.load_workbook(dados_xlsx)
         aba = workbook.active
 
+    # Verifica duplicidade de dados
+    for linha in aba.iter_rows(min_row=2, values_only=True):
+        if linha == (nome, idade, cargo):
+            print(f"Registro duplicado! Não adicionado: {linha}")
+            workbook.close()
+            return
+    
     # Adicionar nova linha de dados
     aba.append([nome, idade, cargo])
     workbook.save(dados_xlsx)
