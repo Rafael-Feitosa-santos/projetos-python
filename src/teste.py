e3ec9f69e57ce47f3398cac077833deeac041288
Rafael Feitosa <rafaelfeit@hotmail.com>
2026-01-28 12:37:13 -0300

    Atualização do codigo arquivo 'teste'

--- src/teste.py
+++ src/teste.py
@@ -8,19 +8,24 @@ while True:
 
     if escolha == '1':
         os.system('cls')
-        adicionar = input("Insira o produto: ")
+        adicionar = input("Insira o produto: ").title()
         lista.append(adicionar)
 
     elif escolha == '2':
         os.system('cls')
-        deletar = input("Deletar produto: ")
-        lista.remove(deletar)
+        deletar = input("Deletar produto: ").title()
+        if deletar not in lista:
+            print("Produto não encontrado")
+        else:
+            lista.remove(deletar)
 
     elif escolha == '3':
         os.system('cls')
-        enumerar = enumerate(lista)
-        for item in enumerar:
-            print(item)
-
+        enumerar = enumerate(lista, start=1)
+        if lista:
+            for indice, item in enumerar:
+                print(f"{indice} - {item}")
+        else:
+            print(" ######## Lista está vázia!  ########")
     else:
         print("Nenhuma opção foi escolhida.")
