6cf5412b96187aa7188578858ab6fcf117dafbdf
Rafael Feitosa <rafaelfeit@hotmail.com>
2026-04-12 00:56:11 -0300

    atualizacao arquivo conta bancaria

--- src/conta_bancaria.py
+++ src/conta_bancaria.py
@@ -1,23 +1,49 @@
+from datetime import datetime, timedelta
+
+
+def horario_brasilia():
+    hora = datetime.now()
+    horario_brasilia = hora
+    horario_brasilia_formataca = horario_brasilia.strftime("%H:%M - %d/%m/%y")
+    return horario_brasilia_formataca
+
+
+
 def validar_entrada(valor):
     return float(valor.replace(",", "."))
 
 
-menu = '''
+def formatacao_valor(valor):
+    valor_formatado = f"{valor:,.2f}".replace(",", ".")
+    valor_formatado = valor_formatado[::-1].replace(".", ",", 1)[::-1]
+    return valor_formatado
+
+
+def menu():
+    menu = """
+==============================
+       MENU DE OPERAÇÕES
+==============================
 [1] Depositar
 [2] Sacar
 [3] Extrato
 [4] Sair
-'''
+==============================
+    """
+    print(menu)
+    return menu
+
 
 saldo = 0
-limite = 500
+limite = 2000
 extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 5
 
 while True:
+
     try:
-        print(menu)
+        menu()
         opcao = int(input("Informe a operação que deseja fazer: "))
 
         if opcao == 1:
@@ -25,9 +51,12 @@ while True:
 
             if valor > 0:
                 saldo += valor
-                extrato += f"Depósito de R$ {valor:.2f}\n"
+                if valor > 1000:
+                    extrato += f"Depósito de R$ {formatacao_valor(valor)} - {horario_brasilia()}\n"
+                else:
+                    extrato += f"Depósito de R$ {formatacao_valor(valor)} - {horario_brasilia()}\n"
             else:
-                print("Operação falhou! O valor informado é inválido.")
+                print("\033[31mOperação falhou! O valor informado é inválido.\033[0m")
 
         elif opcao == 2:
             valor = validar_entrada(input("Informe o valor que deseja sacar: "))
@@ -37,37 +66,45 @@ while True:
             excedeu_limite = valor > limite
 
             if excedeu_saque:
-                print("Operação falhou! Número máximo de saques excedido.")
+                print("\033[31mOperação falhou! Número máximo de saques excedido.\033[0m")
 
             elif excedeu_saldo:
-                print("Operação falhou! Você não tem saldo suficiente.")
+                print("\033[31mOperação falhou! Você não tem saldo suficiente.\033[0m")
 
             elif excedeu_limite:
-                print("Operação falhou! O valor excedeu o limite permitido por saque.")
+                print("\033[31mOperação falhou! O valor excedeu o limite permitido por saque.\033[0m")
 
             elif valor > 0:
                 saldo -= valor
-                extrato += f"Saque de R$ {valor:.2f}\n"
+                if valor > 1000:
+                    extrato += f"Saque de - R$ {formatacao_valor(valor)} - {horario_brasilia()}\n"
+                else:
+                    extrato += f"Saque de - R$ {formatacao_valor(valor)} - {horario_brasilia()}\n"
+
                 numero_saques += 1
             else:
-                print("Operação falhou! O valor informado é inválido.")
+                print("\033[31mOperação falhou! O valor informado é inválido.\033[0m")
 
         elif opcao == 3:
-            print(" Extrato ".center(45, "*"))
+            print()
+            print(" Extrato ".center(50, "*"))
             print("Não foram realizadas movimentações." if not extrato else extrato)
-            print(f"\nSaldo: R$ {saldo:.2f}")
-            print("".center(45, "*"))
+            if saldo > 1000:
+                print(f"\nSaldo: R$ {formatacao_valor(saldo)}")
+            else:
+                print(f"\nSaldo: R$ {formatacao_valor(saldo)}")
+            print("".center(50, "*"))
 
         elif opcao == 4:
             print("Saindo do sistema.")
             break
 
         else:
-            print("Opção inválida! Por favor, selecione novamente a operação desejada.")
+            print("\033[31mOpção inválida! Por favor, selecione novamente a operação desejada.\033[0m")
 
     except KeyboardInterrupt:
         print("\nEncerrado pelo usuário.")
         break
 
     except ValueError:
-        print("Entrada inválida! Por favor, insira um número.")
+        print("\033[31mEntrada inválida! Por favor, insira um número.\033[0m")
